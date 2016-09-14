4fa3ea717fcc3520f16bf89b8e587fbb92532dc7
Eduardo <publicentrosanfelipe@hotmail.com>
2016-09-13 20:24:05 -0500

    tarea 1 terminada

--- angular/js/script.js
+++ angular/js/script.js
@@ -9,20 +9,15 @@
                 if($scope.lunchList===undefined){
                     $scope.result="Please enter data first";
                 }else{
-                $scope.matches =0
-                $scope.matches=count($scope.lunchList)+1;
-                if ($scope.matches > 3) {
-                    $scope.result = "Too much!";
-                }else{
-                    $scope.result = "Enjoy!";
-                }; 
-            };
-        };
-      
-      var count = function(str){
-        var matches = str.match(/.*?,/g);
-        return matches.length;        
-      };
-    });      
+                    var cuenta = $scope.lunchList;
+                    var lista = cuenta.split(",");
+                    if (lista.length > 3) {
+                        $scope.result = "Too much!";
+                     }else{
+                        $scope.result = "Enjoy!";
+                    }; 
+                 };
+            };      
+       });      
 
 })();
